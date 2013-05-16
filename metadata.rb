2aeebd2596e26209353a6d829f87facb8602ec02
Bryan Stenson <bryan.stenson@gmail.com>
2013-05-16 11:00:25 -0700

    fixing doc typo

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures PowerShell 2.0 on the Windows platform"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.8"
+version          "99.99.99"
 
 recipe "powershell::default", "Installs and configures PowerShell 2.0"
 
