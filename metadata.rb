28eacfc988d6708edb542f2eade9091eec0974b3
Julian C. Dunn <jdunn@getchef.com>
2014-05-09 21:33:29 -0400

    Update metadata version for v3.0.4

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,13 @@ maintainer_email 'cookbooks@getchef.com'
 license          'Apache 2.0'
 description      'Installs/Configures PowerShell on the Windows platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '3.0.3'
+version          '3.0.4'
 
 recipe 'powershell::default', 'Does nothing; choose the right version of Powershell by selecting the correct recipe'
 recipe 'powershell::powershell2', 'Installs PowerShell 2.0'
 recipe 'powershell::powershell3', 'Installs PowerShell 3.0'
 recipe 'powershell::powershell4', 'Installs PowerShell 4.0'
+recipe 'powershell::powershell5', 'Installs PowerShell 5.0'
 
 supports         'windows'
 depends          'windows', '>= 1.2.8'
