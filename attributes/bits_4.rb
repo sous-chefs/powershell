40b20ef43dee1a9c9c6a52c7fce49ec7bfdaa2c0
Jeremy MAURO <j.mauro@criteo.com>
2014-05-20 10:05:24 +0200

    Changing Copyright

--- attributes/bits_4.rb
+++ attributes/bits_4.rb
@@ -1,9 +1,20 @@
+#
+# Author:: Jeremy MAURO (<j.mauro@criteo.com>)
 # Cookbook Name:: powershell
-# Description::   Setup attributs for bits_4
-# Recipe::        bits_4
-# Author::        Jeremy MAURO (j.mauro@criteo.com)
+# Attributes:: bits_4
+# Copyright:: Copyright (c) 2014 Jeremy MAURO
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 if node['kernel']['machine'] == 'i386'
