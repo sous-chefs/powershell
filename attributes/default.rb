b644a81ba2f290db4dfbdce0b00f11f46d08b997
nimisha <nimisha.sharad@clogeny.com>
2014-11-26 10:32:59 +0000

    Replaced %x by powershell resource to enable https transport

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,7 +22,9 @@ if node['platform_family'] == 'windows'
   # INSTALLATION_REBOOT_MODE = "immediate_reboot". Used for immediate node reboot after powershell installation.
   # INSTALLATION_REBOOT_MODE = "delayed_reboot". Used for node reboot after chef-client run.
   default['powershell']['installation_reboot_mode'] = ENV['INSTALLATION_REBOOT_MODE'] || 'no_reboot'
-  default['powershell']['winrm']['enable_https_transport'] = false
-  default['powershell']['winrm']['thumbprint'] = "75A3C92D9D86BB721C3AFED6A86F56C464213E9F"
+
+  # For enabling HTTPS transport in dsc recipe
+  default['powershell']['winrm']['enable_https_transport'] = true
+  default['powershell']['winrm']['thumbprint'] = "75A3C92D9D86BB721C3AFED6A86F56C464213E9F"	#change this
   default['powershell']['winrm']['hostname'] = ""
 end
