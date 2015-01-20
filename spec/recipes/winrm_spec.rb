599046b04c78a095eb45447e2445f022ca722c54
nimisha <nimisha.sharad@clogeny.com>
2015-01-20 16:48:09 +0530

    Fixed chefspec

--- spec/recipes/winrm_spec.rb
+++ spec/recipes/winrm_spec.rb
@@ -3,10 +3,10 @@ require 'chef/win32/version'
 
 describe 'powershell::winrm' do
   let(:chef_run) do
-    ChefSpec::Runner.new(platform: 'windows', version: '2012').converge(described_recipe)
+    ChefSpec::SoloRunner.new(platform: 'windows', version: '2012').converge(described_recipe)
   end
 
   it 'installs windows package windows managemet framework core 5.0' do
-    expect(chef_run).to run_powershell('enable winrm').with(code: "      winrm quickconfig -q\n")
+    expect(chef_run).to run_powershell_script('enable winrm').with(code: "      winrm quickconfig -q\n")
   end
 end
