a7d14e2165ce9f43441ccc61a3bf9db6e9685074
Baptiste Courtois <b.courtois@criteo.com>
2015-11-28 16:13:59 +0100

    Fix specs for winrm recipe
    Update winrm's specs to be able to test on linux/travis:
    * Remove references to `chef/winr32/version`
    * Mock shellout commands

--- spec/recipes/winrm_spec.rb
+++ spec/recipes/winrm_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
-require 'chef/win32/version'
 
 describe 'powershell::winrm' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new(platform: 'windows', version: '2012').converge(described_recipe)
+  end
+
+  before do
+    winrm_cmd = double('winrm_cmd', run_command: nil, stdout: 'Transport = HTTPS')
+    allow(Mixlib::ShellOut).to receive(:new).with('powershell.exe winrm enumerate winrm/config/listener').and_return winrm_cmd
   end
 
   it 'installs windows package windows managemet framework core 5.0' do
