3f146f7c6a87ae2e82895ca7ae22b1524941a47e
Steven Murawski <steven.murawski@gmail.com>
2015-12-23 09:31:06 -0600

    support .kitchen.cloud.yml

--- test/cookbooks/minimal/recipes/default.rb
+++ test/cookbooks/minimal/recipes/default.rb
@@ -1,24 +1,6 @@
-#
-# powershell_module 'posh-git' do
-#   package_name 'posh-git'
-#   source 'https://github.com/dahlbyk/posh-git/zipball/master'
-# end
+include_recipe 'powershell::default'
 
-include_recipe 'powershell::powershell5'
-
-
-powershell_package_source 'PSGallery' do
-  action :update
+powershell_module 'posh-git' do
+  package_name 'posh-git'
+  source 'https://github.com/dahlbyk/posh-git/zipball/master'
 end
-
-powershell_dsc_module 'xTimeZone' do
-  action :install
-end
-
-# powershell_dsc_module 'xTimeZone' do
-#   action :update
-# end
-
-# powershell_dsc_module 'xTimeZone' do
-#   action :uninstall
-# end
