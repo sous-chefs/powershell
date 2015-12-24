2285fd9adde6187f80e1c77cd58b9a91ffeec3cb
Steven Murawski <steven.murawski@gmail.com>
2015-12-24 00:32:22 -0600

    Revert to WMF5 Prod Preview

--- attributes/powershell5.rb
+++ attributes/powershell5.rb
@@ -19,32 +19,32 @@
 #
 
 if node['platform_family'] == 'windows'
-  default['powershell']['powershell5']['version'] = '5.0.10586.51'
+  default['powershell']['powershell5']['version'] = '5.0.10514.6'
 
   case node['platform_version'].split('.')[0..1].join('.')
   when '6.1'
     case node['kernel']['machine']
     when 'i386'
-      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win7-KB3094176-x86.msu'
-      default['powershell']['powershell5']['checksum'] = 'dfac22163aa6c51eaf0f0ae59a3f9632558aa08229350d0f272d742bd02f7109'
+      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/3/F/D/3FD04B49-26F9-4D9A-8C34-4533B9D5B020/Win7AndW2K8R2-KB3066439-x86.msu'
+      default['powershell']['powershell5']['checksum'] = '8f019d7444b0995fe78bfc41115535c1f08ed9b08cd80f188d148bcd6c29d236'
     when 'x86_64'
-      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/W2K8R2-KB3094176-x64.msu'
-      default['powershell']['powershell5']['checksum'] = '22d4a4ff739a3734e1efa410bd9f745c668c31efeeffa170f7968d42022c641f'
+      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/3/F/D/3FD04B49-26F9-4D9A-8C34-4533B9D5B020/Win7AndW2K8R2-KB3066439-x64.msu'
+      default['powershell']['powershell5']['checksum'] = '1c068cb6e342c2bc789bb009bc50d1bddc37e313106f696521c0b27b7cec3364'
     end
   when '6.2'
     case node['kernel']['machine']
     when 'x86_64'
-      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/W2K12-KB3094175-x64.msu'
-      default['powershell']['powershell5']['checksum'] = 'cfda8048978708fe4f31adb6c045e848db4af5decf88f64e38aa511db92e1d49'
+      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/3/F/D/3FD04B49-26F9-4D9A-8C34-4533B9D5B020/W2K12-KB3066438-x64.msu'
+      default['powershell']['powershell5']['checksum'] = '281d85ec2317240f260f6a42c2c5c9dfbddfdb3bc361950f1ec29a7c06b8c857'
     end
   when '6.3'
     case node['kernel']['machine']
     when 'i386'
-      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/Win8.1-KB3094174-x86.msu'
-      default['powershell']['powershell5']['checksum'] = '8e3a1414e57211623c2a6097dbdf982855f2e53fee65859dfa36891bf0be8e87'
+      default['powershell']['powershell5']['url'] = 'http://download.microsoft.com/download/3/F/D/3FD04B49-26F9-4D9A-8C34-4533B9D5B020/Win8.1AndW2K12R2-KB3066437-x86.msu'
+      default['powershell']['powershell5']['checksum'] = '0810a0eebf2239adde959561be8550f923ffb00e8b7d3a843143261937a0a5ab'
     when 'x86_64'
-      default['powershell']['powershell5']['url'] = 'https://download.microsoft.com/download/2/C/6/2C6E1B4A-EBE5-48A6-B225-2D2058A9CEFB/W2K12R2-KB3094174-x64.msu'
-      default['powershell']['powershell5']['checksum'] = 'cfda8048978708fe4f31adb6c045e848db4af5decf88f64e38aa511db92e1d49'
+      default['powershell']['powershell5']['url'] = 'http://download.microsoft.com/download/3/F/D/3FD04B49-26F9-4D9A-8C34-4533B9D5B020/Win8.1AndW2K12R2-KB3066437-x64.msu'
+      default['powershell']['powershell5']['checksum'] = '9c57302ff0515a6b7eb53ab07bed0f5d420bd7204296d9f3fd17452fca1d5b3d'
     end
   end
 end
