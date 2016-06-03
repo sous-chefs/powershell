c0e1f8bbfc7adc7464e7cc321572dbb84fdb1341
Maksim Horbul <max@gorbul.net>
2016-06-02 18:34:35 -0700

    remove deprecation warning https://github.com/chef/chef/commit/97aaf5bbcdfd0810722b123bdc67e883a7ca8077

--- libraries/powershell_module_resource.rb
+++ libraries/powershell_module_resource.rb
@@ -22,7 +22,7 @@ require_relative 'powershell_module_provider'
 class PowershellModule < Chef::Resource::Package
   state_attrs :enabled
 
-  provides :powershell_module, on_platforms: ['windows']
+  provides :powershell_module, platform: ['windows']
 
   def initialize(name, run_context = nil)
     super
