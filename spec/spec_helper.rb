a8be326a423e6773c73af72c7714e187f01589c4
Tim Smith <tsmith@chef.io>
2016-08-31 17:34:57 -0700

    Avoid deprecation warnings during and be more verbose
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,9 @@ end
 
 # Require all our libraries
 Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
+
+RSpec.configure do |config|
+  config.color = true               # Use color in STDOUT
+  config.formatter = :documentation # Use the specified formatter
+  config.log_level = :error         # Avoid deprecation notice SPAM
+end
