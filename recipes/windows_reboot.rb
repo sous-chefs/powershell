a4f823ef22e893f5e049f0899e83fd5d83b720db
Jason Morgan <jmorgan@f9vs.com>
2016-09-25 16:19:45 -0400

    Removed the notifies attribute and the end_chef_run ruby block.

--- recipes/windows_reboot.rb
+++ recipes/windows_reboot.rb
@@ -4,14 +4,4 @@ node.default['windows']['allow_reboot_on_failure'] = true
 
 reboot 'powershell' do
   action :nothing
-  notifies :run, 'ruby_block[end_chef_run]', :immediately if node['powershell']['installation_reboot_mode'] == 'immediate_reboot'
-end
-
-# The reboot handler only does something at the end of the chef run. If it
-# needs to happen straight away to be useful, throw an exception...
-ruby_block 'end_chef_run' do
-  block do
-    raise 'Requested sudden end to the run... I hope this was justified.'
-  end
-  action :nothing
 end
