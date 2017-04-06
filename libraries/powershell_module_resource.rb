a3cafec628133e81435c4247a15a8761d14c83fc
Tim Smith <tsmith@chef.io>
2017-04-05 22:22:25 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/powershell_module_resource.rb
+++ libraries/powershell_module_resource.rb
@@ -1,7 +1,7 @@
 #
 # Author:: Siddheshwar More (<siddheshwar.more@clogeny.com>)
 #
-# Copyright:: 2014-2016, Chef Software, Inc
+# Copyright:: 2014-2017, Chef Software, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
