6e468008b8bdd146de243caafc935f09a80fb9c0
Bob Dean <radean@hotmail.com>
2017-06-13 15:19:43 -0400

    chefspec tests for wmf51
    Signed-off-by: Bob Dean <radean@hotmail.com>

--- spec/recipes/powershell5_spec.rb
+++ spec/recipes/powershell5_spec.rb
@@ -14,16 +14,16 @@ describe 'powershell::powershell5' do
       end
 
       let(:chef_run) do
-        ChefSpec::SoloRunner.new(platform: 'windows', version: test_conf[:fauxhai_version]) do |node|
+        ChefSpec::SoloRunner.new(platform: 'windows', version: test_conf[:fauxhai_version], file_cache_path: 'c:\Chef' ) do |node|
           node.automatic['kernel']['os_info']['product_type'] = test_conf[:product_type] if test_conf[:product_type]
           node.normal['powershell']['powershell5']['url'] = 'https://powershelltest.com'
           node.normal['powershell']['powershell5']['checksum'] = '12345'
         end.converge(described_recipe)
       end
 
-      it 'includes powershell 2 recipe' do
+      it 'includes ms_dotnet 4 recipe' do
         allow(::Powershell::VersionHelper).to receive(:powershell_version?).and_return false
-        expect(chef_run).to include_recipe('powershell::powershell2')
+        expect(chef_run).to include_recipe('ms_dotnet::ms_dotnet4')
       end
 
       context 'when powershell is installed' do
@@ -31,8 +31,8 @@ describe 'powershell::powershell5' do
           allow(::Powershell::VersionHelper).to receive(:powershell_version?).and_return true
         end
 
-        it 'does not install WMF 5' do
-          expect(chef_run).to_not install_windows_package('Windows Management Framework Core 5.0')
+        it 'does not install WMF 5.1' do
+          expect(chef_run).to_not install_windows_package('Windows Management Framework Core 5.1')
         end
       end
 
@@ -41,8 +41,14 @@ describe 'powershell::powershell5' do
           allow(::Powershell::VersionHelper).to receive(:powershell_version?).and_return false
         end
 
-        it 'installs windows package windows management framework core 5.0' do
-          expect(chef_run).to install_windows_package('Windows Management Framework Core 5.0').with(source: 'https://powershelltest.com', checksum: '12345', installer_type: :custom, options: '/quiet /norestart', timeout: test_conf[:timeout])
+        if windows_version == 'Windows Server 2008R2'
+          it 'installs windows package windows management framework core 5.1' do
+            expect(chef_run).to install_windows_package('Windows Management Framework Core 5.1').with(source: Chef::Util::PathHelper.canonical_path("#{Chef::Config['file_cache_path']}\\wmf51\\Win7AndW2K8R2-KB3191566-x64.msu", false), installer_type: :custom, options: '/quiet /norestart', timeout: test_conf[:timeout])
+          end
+        else
+          it 'installs windows package windows management framework core 5.1' do
+            expect(chef_run).to install_windows_package('Windows Management Framework Core 5.1').with(source: 'https://powershelltest.com', checksum: '12345', installer_type: :custom, options: '/quiet /norestart', timeout: test_conf[:timeout])
+          end
         end
       end
     end
